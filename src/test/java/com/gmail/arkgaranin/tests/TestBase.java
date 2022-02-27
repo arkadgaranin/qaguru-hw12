104dcd33d254911d6f3c98f00b19ac9a6cd4519e
Arkadiy <arkadiy.garanin@socialsys.ru>
2022-02-27 14:34:37 +0300

    Добавил метод addAttachments в TestBase

--- src/test/java/com/gmail/arkgaranin/tests/TestBase.java
+++ src/test/java/com/gmail/arkgaranin/tests/TestBase.java
@@ -1,6 +1,8 @@
 package com.gmail.arkgaranin.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.gmail.arkgaranin.helpers.Attachments;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -13,5 +15,13 @@ public class TestBase {
     Configuration.browserCapabilities = options;
     Configuration.baseUrl = "https://msk.tele2.ru";
     Configuration.browserSize = "1920x1080";
+  }
+
+  @AfterEach
+  void addAttachments() {
+    Attachments.takeScreenshot();
+    Attachments.takePageSource();
+    Attachments.addVideo();
+    Attachments.browserConsoleLogs();
   }
 }
