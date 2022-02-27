872fb0a2585127f7fb9c89f3abfa7bd2cc1a18b8
Arkadiy <arkadiy.garanin@socialsys.ru>
2022-02-27 14:28:26 +0300

    Написал 4 теста для сайта tele2

---
+++ src/test/java/com/gmail/arkgaranin/tests/Tele2Tests.java
@@ -0,0 +1,80 @@
+package com.gmail.arkgaranin.tests;
+
+import com.gmail.arkgaranin.pages.MainPage;
+import com.gmail.arkgaranin.pages.MyOnlinePage;
+import com.gmail.arkgaranin.pages.TariffsPage;
+import com.gmail.arkgaranin.pages.TrashPage;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Tele2Tests extends TestBase {
+
+  MainPage mainPage = new MainPage();
+  TariffsPage tariffsPage = new TariffsPage();
+  MyOnlinePage myOnlinePage = new MyOnlinePage();
+  TrashPage trashPage = new TrashPage();
+
+  @Test
+  void purchaseSimCardTest() {
+    mainPage
+        .openPage()
+        .gotoTariffsPage();
+
+    tariffsPage.choiceTariff();
+
+    myOnlinePage
+        .checkPageTitleForTariff()
+        .choiceOneSimCard()
+        .clickOnSubmitBtn()
+        .submitPurchase();
+
+    trashPage.сheckTariffIsOnTrashPage();
+  }
+
+  @Test
+  void deleteSimFromTrashTest() {
+    mainPage
+        .openPage()
+        .gotoTrash();
+
+    trashPage.atPage()
+        .deleteSimCard()
+        .сheckThatTrashIsEmpty();
+  }
+
+  @Test
+  void searchForFreeNumberTest() {
+    mainPage
+        .openPage()
+        .gotoNumbersPage();
+
+    $("h1.catalog-header").shouldHave(text("Выберите номер телефона"));
+    $("#searchNumber").setValue("9772601157");
+    $("div.empty-list").shouldHave(text("По вашему запросу ничего не нашлось"));
+
+    $("a.reset-filters").click();
+    $("div > a.short-info").shouldHave(text("Показать еще"));
+    $("#searchNumber").setValue("901 369-68-77");
+    $("div.catalog-numbers").shouldHave(text("901 369-68-77"));
+  }
+
+  @Test
+  void searchSmartphonesByFiltersTest() {
+    mainPage
+        .openPage()
+        .gotoSmartphonesPage();
+
+    $("div.products-header").shouldHave(text("Смартфоны"));
+    $("#radio_e43fd26858c53d4ef093f0f8d77648d6_listbox > li:nth-child(1) > label > span").click();
+    $(".products-filter > div:nth-of-type(6) .filter-title").click();
+    $("[for='checkBoxField_Samsung']").click();
+    $(".products-filter > div:nth-of-type(7) .filter-title").click();
+    $("[for='checkBoxField_синий']").click();
+    $(".products-filter > div:nth-of-type(8) .filter-title").click();
+    $("[for='checkBoxField_32 Гб']").click();
+    $$(".products-list .col-xs-12").shouldHave(size(5));
+  }
+}
