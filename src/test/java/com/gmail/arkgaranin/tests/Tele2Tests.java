e23d6577a68073188b98c77bf3f4e58370cf8275
Arkadiy <arkadiy.garanin@socialsys.ru>
2022-03-13 17:32:21 +0300

    Закомментил временно 2 теста

--- src/test/java/com/gmail/arkgaranin/tests/Tele2Tests.java
+++ src/test/java/com/gmail/arkgaranin/tests/Tele2Tests.java
@@ -54,42 +54,42 @@ public class Tele2Tests extends TestBase {
         .сheckThatTrashIsEmpty();
   }
 
-  @Link(name = "Cтр-ца сайта tele2", url = "https://msk.tele2.ru")
-  @DisplayName("Поиск свободного номера телефона")
-  @Severity(SeverityLevel.NORMAL)
-  @Test
-  void searchForFreeNumberTest() {
-    mainPage
-        .openPage()
-        .gotoNumbersPage();
-
-    $("h1.catalog-header").shouldHave(text("Выберите номер телефона"));
-    $("#searchNumber").setValue("9772601157");
-    $("div.empty-list").shouldHave(text("По вашему запросу ничего не нашлось"));
-
-    $("a.reset-filters").click();
-    $("div > a.short-info").shouldHave(text("Показать еще"));
-    $("#searchNumber").setValue("9772516671");
-    $("div.catalog-numbers").shouldHave(text("977 251-66-71"));
-  }
-
-  @Link(name = "Cтр-ца сайта tele2", url = "https://msk.tele2.ru")
-  @DisplayName("Поиск смартфона, используя фильтры")
-  @Severity(SeverityLevel.NORMAL)
-  @Test
-  void searchSmartphonesByFiltersTest() {
-    mainPage
-        .openPage()
-        .gotoSmartphonesPage();
-
-    $("div.products-header").shouldHave(text("Смартфоны"));
-    $("#radio_e43fd26858c53d4ef093f0f8d77648d6_listbox > li:nth-child(1) > label > span").click();
-    $(".products-filter > div:nth-of-type(6) .filter-title").click();
-    $("[for='checkBoxField_Samsung']").click();
-    $(".products-filter > div:nth-of-type(7) .filter-title").click();
-    $("[for='checkBoxField_синий']").click();
-    $(".products-filter > div:nth-of-type(8) .filter-title").click();
-    $("[for='checkBoxField_32 Гб']").click();
-    $$(".products-list .col-xs-12").shouldHave(size(5));
-  }
+//  @Link(name = "Cтр-ца сайта tele2", url = "https://msk.tele2.ru")
+//  @DisplayName("Поиск свободного номера телефона")
+//  @Severity(SeverityLevel.NORMAL)
+//  @Test
+//  void searchForFreeNumberTest() {
+//    mainPage
+//        .openPage()
+//        .gotoNumbersPage();
+//
+//    $("h1.catalog-header").shouldHave(text("Выберите номер телефона"));
+//    $("#searchNumber").setValue("9772601157");
+//    $("div.empty-list").shouldHave(text("По вашему запросу ничего не нашлось"));
+//
+//    $("a.reset-filters").click();
+//    $("div > a.short-info").shouldHave(text("Показать еще"));
+//    $("#searchNumber").setValue("9772516671");
+//    $("div.catalog-numbers").shouldHave(text("977 251-66-71"));
+//  }
+//
+//  @Link(name = "Cтр-ца сайта tele2", url = "https://msk.tele2.ru")
+//  @DisplayName("Поиск смартфона, используя фильтры")
+//  @Severity(SeverityLevel.NORMAL)
+//  @Test
+//  void searchSmartphonesByFiltersTest() {
+//    mainPage
+//        .openPage()
+//        .gotoSmartphonesPage();
+//
+//    $("div.products-header").shouldHave(text("Смартфоны"));
+//    $("#radio_e43fd26858c53d4ef093f0f8d77648d6_listbox > li:nth-child(1) > label > span").click();
+//    $(".products-filter > div:nth-of-type(6) .filter-title").click();
+//    $("[for='checkBoxField_Samsung']").click();
+//    $(".products-filter > div:nth-of-type(7) .filter-title").click();
+//    $("[for='checkBoxField_синий']").click();
+//    $(".products-filter > div:nth-of-type(8) .filter-title").click();
+//    $("[for='checkBoxField_32 Гб']").click();
+//    $$(".products-list .col-xs-12").shouldHave(size(5));
+//  }
 }
